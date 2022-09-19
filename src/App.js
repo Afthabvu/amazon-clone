301c7a5dd432190e4959187ad0bfaa0fa08ac05d
Afthab <afthabvu@gmail.com>
2022-09-19 19:12:10 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 function App() {
   return (
     <div>
       <h1>hello</h1>
       <h1>hellohi</h1>
+      <h1>hi</h1>
     </div>
   );
 }
