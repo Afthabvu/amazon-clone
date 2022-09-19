7bb3edb7e0b73fe5643c9e6d823f974dadf6955c
Afthab <afthabvu@gmail.com>
2022-09-19 14:00:16 +0530

    React app setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLeVAJqJWffNBgXPNhAsYcuzj0B2sswRM",
+  authDomain: "fir-7e353.firebaseapp.com",
+  projectId: "fir-7e353",
+  storageBucket: "fir-7e353.appspot.com",
+  messagingSenderId: "972763144797",
+  appId: "1:972763144797:web:3f9c851620b8fe0d6c23b4",
+  measurementId: "G-PQMM9NYD5N"
+};
