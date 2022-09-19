dc8038af1dbbf6da9fb2c946ea5a07ee714a8db1
Afthab <afthabvu@gmail.com>
2022-09-19 23:14:11 +0530

    Header styling completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+
 import "./App.css";
+import Header from "./components/Header";
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-      <h1>hellohi</h1>
-      <h1>hi</h1>
+    <div className="app">
+
+      <Header />
+
     </div>
   );
 }
